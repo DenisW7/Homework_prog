5c36ad63867abeaab05c85e2194e48d07af9851b
DenisW7 <denis2017popow@mail.ru>
2026-03-28 21:21:24 +0300

    Создал код, находящий детерминант матрицы 3x3

---
+++ practice-4/practice-4.py
@@ -0,0 +1,17 @@
+def determinant(matrix):
+    det = (matrix[0][0] * matrix[1][1] * matrix[2][2] 
+    + matrix[1][0] * matrix[2][1] * matrix[0][2] 
+    + matrix[2][0] * matrix[0][1] * matrix[1][2] 
+    - matrix[2][0] * matrix[1][1] * matrix[0][2] 
+    - matrix[1][0] * matrix[0][1] * matrix[2][2]
+    - matrix[0][0] * matrix[2][1] * matrix[1][2])
+    
+    return det
+
+
+matrix = [[1, 2, 3], 
+          [4, 5, 6], 
+          [7, 8, 9]]
+
+det = determinant(matrix)
+print(f"Детерминант матрицы: {det}")
