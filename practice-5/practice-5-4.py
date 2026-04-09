8280986a1c57eefd6ee331b49f8421466145daa5
DenisW7 <denis2017popow@mail.ru>
2026-04-09 18:08:48 +0300

    Добавил ответ на 4 пункт задания 5

---
+++ practice-5/practice-5-4.py
@@ -0,0 +1,17 @@
+def bubble_sort(arr):
+    if not arr:
+        return []
+    
+    result = arr.copy()
+    n = len(result)
+    
+    for i in range(n):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if result[j] > result[j + 1]:
+                result[j], result[j + 1] = result[j + 1], result[j]
+                swapped = True
+        if not swapped:
+            break
+    
+    return result
